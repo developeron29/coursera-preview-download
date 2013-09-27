4e04fd8294b0e410627a8d25cf3f40ac2d2c3aad
Ayush Narula <developeron29@gmail.com>
2013-09-27 09:53:15 +0530

    Update coursera-preview-download.sh
    valid for all courses

--- coursera-preview-download.sh
+++ coursera-preview-download.sh
@@ -1,7 +1,7 @@
 lynx -dump -listonly $1 >> a.txt
 sed "s/preview_view\//download.mp4\?lecture_id=/g" a.txt >> b.txt
 (cat b.txt |
-grep -oh "https://class.coursera.org/[a-z]*/lecture/download.mp4?lecture_id=[0-9]*" |
+grep -oh "https://class.coursera.org/.*/lecture/download.mp4?lecture_id=[0-9]*" |
 while read a;do
 wget --content-disposition ${a}
 done
